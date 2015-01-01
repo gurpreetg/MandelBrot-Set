5ab7cc8a8be4b40e5e17306f6c515b01815994db
Gurpreet Gill <gurpreetgill42@gmail.com>
2015-01-01 18:05:50 -0500

    Uploading Project

---
+++ MandelbrotSet/MandelbrotSetForm.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace MandelbrotSetApplication
+{
+    partial class MandelbrotSetForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mandelbrotSetPictureBox = new System.Windows.Forms.PictureBox();
+            this.displayMandelbrotSetButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.colouringMethodComboBox = new System.Windows.Forms.ComboBox();
+            this.mouseCoordinatesLabel = new System.Windows.Forms.Label();
+            this.complexPlaneCoordinatesLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.mandelbrotSetPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mandelbrotSetPictureBox
+            // 
+            this.mandelbrotSetPictureBox.Location = new System.Drawing.Point(0, -1);
+            this.mandelbrotSetPictureBox.Name = "mandelbrotSetPictureBox";
+            this.mandelbrotSetPictureBox.Size = new System.Drawing.Size(800, 600);
+            this.mandelbrotSetPictureBox.TabIndex = 0;
+            this.mandelbrotSetPictureBox.TabStop = false;
+            this.mandelbrotSetPictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.mandelbrotSetPictureBox_Paint);
+            this.mandelbrotSetPictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.mandelbrotSetPictureBox_MouseMove);
+            // 
+            // displayMandelbrotSetButton
+            // 
+            this.displayMandelbrotSetButton.Location = new System.Drawing.Point(4, 606);
+            this.displayMandelbrotSetButton.Name = "displayMandelbrotSetButton";
+            this.displayMandelbrotSetButton.Size = new System.Drawing.Size(187, 45);
+            this.displayMandelbrotSetButton.TabIndex = 1;
+            this.displayMandelbrotSetButton.Text = "Display Mandelbrot Set";
+            this.displayMandelbrotSetButton.UseVisualStyleBackColor = true;
+            this.displayMandelbrotSetButton.Click += new System.EventHandler(this.displayMandelbrotSetButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(205, 621);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Colouring Method";
+            // 
+            // colouringMethodComboBox
+            // 
+            this.colouringMethodComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.colouringMethodComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.colouringMethodComboBox.FormattingEnabled = true;
+            this.colouringMethodComboBox.Location = new System.Drawing.Point(326, 618);
+            this.colouringMethodComboBox.Name = "colouringMethodComboBox";
+            this.colouringMethodComboBox.Size = new System.Drawing.Size(172, 24);
+            this.colouringMethodComboBox.TabIndex = 3;
+            this.colouringMethodComboBox.SelectedValueChanged += new System.EventHandler(this.colouringMethodComboBox_SelectedValueChanged);
+            // 
+            // mouseCoordinatesLabel
+            // 
+            this.mouseCoordinatesLabel.AutoSize = true;
+            this.mouseCoordinatesLabel.Location = new System.Drawing.Point(576, 629);
+            this.mouseCoordinatesLabel.Name = "mouseCoordinatesLabel";
+            this.mouseCoordinatesLabel.Size = new System.Drawing.Size(0, 13);
+            this.mouseCoordinatesLabel.TabIndex = 4;
+            // 
+            // complexPlaneCoordinatesLabel
+            // 
+            this.complexPlaneCoordinatesLabel.AutoSize = true;
+            this.complexPlaneCoordinatesLabel.Location = new System.Drawing.Point(712, 628);
+            this.complexPlaneCoordinatesLabel.Name = "complexPlaneCoordinatesLabel";
+            this.complexPlaneCoordinatesLabel.Size = new System.Drawing.Size(0, 13);
+            this.complexPlaneCoordinatesLabel.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(518, 606);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Mouse Co-ordinates";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(660, 606);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(186, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Complex Plane Co-ordinates";
+            // 
+            // MandelbrotSetForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(846, 662);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.complexPlaneCoordinatesLabel);
+            this.Controls.Add(this.mouseCoordinatesLabel);
+            this.Controls.Add(this.colouringMethodComboBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.displayMandelbrotSetButton);
+            this.Controls.Add(this.mandelbrotSetPictureBox);
+            this.Name = "MandelbrotSetForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.mandelbrotSetPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox mandelbrotSetPictureBox;
+        private System.Windows.Forms.Button displayMandelbrotSetButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox colouringMethodComboBox;
+        private System.Windows.Forms.Label mouseCoordinatesLabel;
+        private System.Windows.Forms.Label complexPlaneCoordinatesLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ MandelbrotSet/MandelbrotSetForm.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+//To access the "Complex" structure, it is necessary to include the following "using" directive.
+//In addition, it is necessary to add a reference to "System.Numerics" through "Project/Add Reference..." (.NET tab)
+using System.Numerics;
+ 
+
+namespace MandelbrotSetApplication
+{
+    
+    /**
+     * Definition of the class 'MandelbrotSetForm.' This class inherits all the members of the 'Form' class.
+     */
+    
+    public partial class MandelbrotSetForm : Form
+    {
+        /**
+         * DATA MEMBERS of the class 'MandelbrotSetForm.'
+         * 
+         * These constants, variables and objects are GLOBAL to the class.
+         * 
+         * Data members can be declared as 'public,' 'private' or 'protected.' The keywords 'public,' 'private' 
+         * and 'protected'are known as "MEMBER ACCESS MODIFIERS" because they define the level of access to the data members.
+         * 
+         * private   -> The member can only be accessed within the class or structure in which it is declared.
+         *              This is the least permissive access level.
+         * public    -> The member can be accessed by any class. There are no restrictions on accessing public members.
+         *              This is the most permissive access level.
+         * protected -> The member is accessible from within the class in which it is declared and and from within any
+         *              class derived from the class that declared this member. This level of access is intermediate
+         *              between 'public' and 'private.'
+         */
+        
+
+        private const int NO_COLOURING=0, ITERATIONS = 1, MODULUS = 2, TRIG = 3;
+
+        private static int colouringMethod = NO_COLOURING;
+        private static double maxModulus = 2.0d;
+        
+        private Bitmap mandelbrotSetBitmap = new Bitmap(800, 600); //The size of the bitmap is set initially to 800x600.
+                
+        
+        
+        /**
+         * Constructor Method for the class 'MandelbrotSetForm.' This method is called as soon as the form object is created.
+         * This constructor should include any statements that need to be executed before the user interacts with the program.
+         */
+        public MandelbrotSetForm()
+        {
+            InitializeComponent();
+
+            //Add items to the object 'colouringMethodComboBox.'
+            colouringMethodComboBox.Text = "None";
+            colouringMethodComboBox.Items.Add("None");
+            colouringMethodComboBox.Items.Add("Iterations Method");
+            colouringMethodComboBox.Items.Add("Modulus Method");
+            colouringMethodComboBox.Items.Add("Trigonometric Method");
+        }
+
+        
+        /*
+         * EVENT HANDLER METHODS
+         */
+                
+        private void displayMandelbrotSetButton_Click(object sender, EventArgs e)
+        {
+
+            Graphics mandelbrotSetImage = Graphics.FromImage(mandelbrotSetBitmap);
+            
+            //Instantiations of the 'Complex' class. Create two complex objects, 'c' and 'z.'
+            //For both 'Complex' objects, the real and imaginary parts are initially set to 0.
+            Complex z = new Complex(0, 0);
+            Complex c =new Complex(0,0);
+
+            //For efficiency reasons, copy the 'Width' and 'Height' properties to variables.
+            //This is called "caching" the values of properties.
+            int width = mandelbrotSetBitmap.Width;
+            int height = mandelbrotSetBitmap.Height;
+
+            mandelbrotSetImage.Clear(mandelbrotSetPictureBox.BackColor);
+            mandelbrotSetPictureBox.Refresh();
+
+            //Traverse the bitmap one pixel at a time, column by column. Screen co-ordinates are (x,y).
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    //The real and imaginary parts of the 'c' are determined by the linear transformations that map
+                    //the region of the Cartesian plane in which 0<=x<800, 0<=y<600 to the region of the Complex plane
+                    //in which -2.5<=Re(z)<1.5 and -1.5<=Im(z)<1.5
+                    c = new Complex(4.0d / width*x - 2.5d, -3.0d / height*y + 1.5d);
+                    z = 0;
+                    int iterations=0;
+                    
+                    //For the value of c corresponding to screen co-ordinates (x,y), generate terms of the Mandelbrot sequence. 
+                    //The loop terminates as soon as |z| exceeds 'maxModulus' or at the 100th term.
+                    do
+                    {
+                        z = Complex.Add(Complex.Pow(z, 2), c);//z=z^2+c
+                        iterations++;
+                    }while(Complex.Abs(z)<maxModulus && iterations<=100);
+
+                    mandelbrotSetBitmap.SetPixel(x, y, pixelColour(colouringMethod, iterations-1, z));
+                    
+                }//end inner for
+            }//end outer for
+
+            //Display the Mandelbrot set by firing the 'Paint' event on 'mandelbrotSetPictureBox.'
+            mandelbrotSetPictureBox.Refresh();
+        }
+
+        private void mandelbrotSetPictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(mandelbrotSetBitmap, 0, 0);
+        }
+
+        private void colouringMethodComboBox_SelectedValueChanged(object sender, EventArgs e)
+        {
+            colouringMethod = colouringMethodComboBox.SelectedIndex;
+            if (colouringMethodComboBox.SelectedIndex == MODULUS)
+                maxModulus = 3.0d;
+            else
+                maxModulus = 2.0d;
+        }
+
+
+        /*
+         * OTHER METHODS FOR INTERNAL USE ONLY
+         */
+
+        private static Color pixelColour(int colourMethod, int repetitions, Complex c)
+        {
+            double modulus = Complex.Abs(c);
+
+            if (colourMethod == ITERATIONS)
+            {
+                if (modulus >= 2.0d)
+                {
+                    int red=(int)(2.55f * repetitions);
+                    int green=red;
+                    int blue=(int)(255 - 2.55f * repetitions);
+                    return Color.FromArgb(red,green,blue);
+                }
+                else
+                    return Color.Black;
+            }
+            else if (colourMethod == MODULUS)
+            {
+                if (modulus >= 2.0d)
+                {
+                    int red = (int)Math.Round(-23.3d * modulus + 277.0d);
+                    int green = (int)Math.Round(10.0d * Math.Pow(modulus, 2) - 136.0d * modulus + 489.0d);
+                    int blue = (int)Math.Round(25.3d * modulus - 50.0d);
+                    return Color.FromArgb(red,green,blue);
+                }
+                else
+                    return Color.Black;
+
+            }
+            else if (colourMethod == TRIG)
+            {
+                int red = 255 - (int)(255 * Complex.Abs(Math.Sin(c.Real)));
+                int blue = 255 - (int)(255 * Complex.Abs(Math.Cos(c.Imaginary)));
+                int green = (red < blue) ? red : blue; //Set green to smaller of red and blue
+                return Color.FromArgb(red, green, blue);
+            }
+            else if (modulus < 2.0d) //
+                return Color.Black;
+            else
+                return Color.White;
+        }
+
+        private void mandelbrotSetPictureBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            mouseCoordinatesLabel.Text = "(" + e.X + ", " + e.Y + ")";
+            complexPlaneCoordinatesLabel.Text = "(" + Math.Round(4.0d / mandelbrotSetBitmap.Width * e.X - 2.5d,2)
+                                                    + ", " + Math.Round(-3.0d / mandelbrotSetBitmap.Height * e.Y + 1.5d, 2) + ")";
+        }
+
+    }
+}
